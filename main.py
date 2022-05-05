ce62c943f10770f40bf211ec0f1bd94340deeb0f
Longfei-CLF <longfei_c@outlook.com>
2022-05-05 19:32:14 +0000

    update linting

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ mssql_dict["database"] = "DSTest"
 mssql_dict["user"] = "candidate"
 mssql_dict["pwd"] = "P)pE8J%XYVdv)4k_K%8]v@f)"
 
-conn = pymssql.connect(mssql_dict["server"],mssql_dict["user"],
-            mssql_dict["pwd"],mssql_dict["database"],charset = 'utf8')
+conn = pymssql.connect(mssql_dict["server"], mssql_dict["user"],
+                       mssql_dict["pwd"], mssql_dict["database"])
 
-sqlstr='''
+sqlstr = '''
 select UserId from dbo.Activity
 '''
 
@@ -19,6 +19,7 @@ with conn.cursor() as cursor:
     cursor.execute(sqlstr)
     data = cursor.fetchall()
     column_names = [item[0] for item in cursor.description]
-    df = pd.DataFrame(list(data), columns = column_names)
+    df = pd.DataFrame(list(data), columns=column_names)
 
 print(df.head())
+
