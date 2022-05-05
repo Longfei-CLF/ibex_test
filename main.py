9927caf86013da1d117d600ef526abc377b9576b
Longfei-CLF <longfei_c@outlook.com>
2022-05-05 18:31:43 +0000

    update venv

---
+++ main.py
@@ -0,0 +1,24 @@
+import pymssql
+import pandas as pd
+
+mssql_dict = dict()
+
+mssql_dict["server"] = "capdstest.database.windows.net"
+mssql_dict["database"] = "DSTest"
+mssql_dict["user"] = "candidate"
+mssql_dict["pwd"] = "P)pE8J%XYVdv)4k_K%8]v@f)"
+
+conn = pymssql.connect(mssql_dict["server"],mssql_dict["user"],
+            mssql_dict["pwd"],mssql_dict["database"],charset = 'utf8')
+
+sqlstr='''
+select UserId from dbo.Activity
+'''
+
+with conn.cursor() as cursor:
+    cursor.execute(sqlstr)
+    data = cursor.fetchall()
+    column_names = [item[0] for item in cursor.description]
+    df = pd.DataFrame(list(data), columns = column_names)
+
+print(df.head())
